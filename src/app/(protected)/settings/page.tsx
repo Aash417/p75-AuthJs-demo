ff2ce0130cfcf1f54aafdaba3038d993bf8a71f3
aashish_k <aashishkathat00@gmail.com>
2024-11-25 17:34:16 +0530

    implemented login

--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -1,8 +1,21 @@
-import { auth } from '@/lib/auth';
-import React from 'react';
+import { Button } from '@/components/ui/button';
+import { auth, signOut } from '@/lib/auth';
 
 export default async function Page() {
    const session = await auth();
 
-   return <div>{JSON.stringify(session)}</div>;
+   return (
+      <div>
+         {JSON.stringify(session)}
+         <form
+            action={async () => {
+               'use server';
+
+               await signOut();
+            }}
+         >
+            <Button type="submit">signout</Button>
+         </form>
+      </div>
+   );
 }
--- src/app/actions/login.ts
+++ src/app/actions/login.ts
@@ -1,6 +1,9 @@
 'use server';
 
+import { DEFAULT_LOGIN_REDIRECT } from '@/route';
 import { LoginSchema } from '@/schemas';
+import { AuthError } from 'next-auth';
+import { signIn } from '@/lib/auth';
 import { z } from 'zod';
 
 export async function login(values: z.infer<typeof LoginSchema>) {
@@ -8,5 +11,28 @@ export async function login(values: z.infer<typeof LoginSchema>) {
    if (!validatedFields.success) return { error: 'Invalid fields' };
 
    console.log(values);
-   return { success: 'submitted' };
+
+   const { email, password } = validatedFields.data;
+
+   try {
+      await signIn('credentials', {
+         email,
+         password,
+         redirectTo: DEFAULT_LOGIN_REDIRECT,
+      });
+
+      return { success: 'logged in' };
+   } catch (error) {
+      if (error instanceof AuthError) {
+         switch (error.type) {
+            case 'CredentialsSignin':
+               return { error: 'Individual credentials' };
+
+            default:
+               return { error: 'Something went wrong' };
+         }
+      }
+
+      throw error;
+   }
 }
